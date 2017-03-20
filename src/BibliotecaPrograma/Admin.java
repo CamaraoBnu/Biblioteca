c783b8e37a7bb47f4b6c70de44608ff7cc1a5b73
tads <tads@atrum.blumenau.ifc.edu.br>
2017-03-20 09:15:40 -0300

    Mudanças

--- src/BibliotecaPrograma/Admin.java
+++ src/BibliotecaPrograma/Admin.java
@@ -1,11 +1,15 @@
 package BibliotecaPrograma;
 
-public class Biblioteca {
+
+public class Admin {
+
+   
 
     public static void main(String[] args) {
         System.out.println("Sistema de biblioteca");
         int i = 0;
         int x = 0;
+       
     }
     
 }
