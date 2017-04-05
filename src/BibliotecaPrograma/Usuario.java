436a9747415b739f28e71637a5c329aca456acc9
informatica <informatica@voziq.blumenau.ifc.edu.br>
2017-04-05 12:30:24 +0000

    ainda no usuario(leeeeeeeeetras nos numeros CPF E TELEFONE)

--- src/BibliotecaPrograma/Usuario.java
+++ src/BibliotecaPrograma/Usuario.java
@@ -31,9 +31,44 @@ public class Usuario {
         System.out.println("Informe o nome da pessoa a ser cadastrada:");
         usuario.nomeUsuario = leia.nextLine();
         MaskFormatter formato = new MaskFormatter();
-        //do{
-            System.out.println("Informe o CPF da respectiva pessoa:");
-       // }while();
+        String aux;
+        char aux1;
+        char aux2;
+        char traco1;
+        boolean erro = false;
+        do {
+            do {
+                System.out.println("Informe o CPF da respectiva pessoa:");
+                aux = leia.nextLine();
+                aux.trim();
+            } while (aux.isEmpty() || (aux.length() != 14));
+            aux1 = aux.charAt(3);
+            aux2 = aux.charAt(7);
+            traco1 = aux.charAt(11);
+
+            if (aux1 == ('.') && (aux2 == ('.')) && (traco1 == ('-'))) {
+                erro = true;
+            } else {
+                erro = false;
+            }
+
+        } while (!erro);
+        String phone;
+
+        do {
+            System.out.println("Informe o seu numero de seu celular(ex:(DDD)91234-5678)");
+            phone = leia.nextLine();
+            if(phone.length()== 15){
+             aux1 = phone.charAt(0);
+            aux2 = phone.charAt(4);
+            traco1 = phone.charAt(10);   
+            erro = false;
+            }else{erro = true;}
+            
+        } while (!((aux1 == '(') && (aux2 == (')')) && (traco1 == ('-')))|| erro);
+
+        System.out.println(aux);
+
         usuario.numCadastro = randomiza();
         System.out.println(numCadastro);
     }
