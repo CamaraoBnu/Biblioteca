1e80c9311e905d9229604d938423021dba00b599
informatica <informatica@aziat.blumenau.ifc.edu.br>
2017-04-10 09:25:56 -0300

    TERMINAMOS IMPLEMNETAÇÃO DE ADMIN E USUARIO

--- src/BibliotecaPrograma/Admin.java
+++ src/BibliotecaPrograma/Admin.java
@@ -34,9 +34,9 @@ public class Admin {
             System.out.println("Informe a senha do admin atual");
             senha = leia.nextLine();
         } while (verificaSenha(nome, senha) == false);
+        System.out.println("USUÁRIO CADASTRADO COM SUCESSO");
         listaAdmin.add(nomenovo);
         senhaAdmin.add(senhanovo);
-        principal();
 
     }
 
@@ -54,80 +54,88 @@ public class Admin {
 
     public static void main(String[] args) {
         System.out.println("BEM VINDO");
-        principal();
+        principal(true);
 
     }
 
     public static void menu() {
         Scanner leia = new Scanner(System.in);
         int resp = 0;
-        System.out.println("");
-        System.out.println("1-Emprestimo");
-        System.out.println("2-Devolução");
-        System.out.println("3-Reserva");
-        System.out.println("4-Cadastrar Exemplar");
-        System.out.println("5-Cadastrar Livro");
-        System.out.println("6-Cadastrar Usuário");
-        System.out.println("7-Cadastrar Admin");
-        System.out.println("8-Sair");
-        System.out.println("");
         do {
-            System.out.println("Escolha a opção desejada");
-            resp = leia.nextInt();
-        } while (resp < 1 || resp > 8);
-        switch (resp) {
-            case 1:
-                Emprestimo emprestimo = new Emprestimo();
-                emprestimo.fazEmprestimo();
-                break;
-            case 2:
-                Devolucao devolucao = new Devolucao();
-                devolucao.fazDevolucao();
-                break;
-            case 3:
-                Reserva reserva = new Reserva();
-                reserva.addUsuarioLista();
-                break;
-            case 4:
-                Exemplar exemplar = new Exemplar();
-                exemplar.cadastraExemplar();
-                break;
-            case 5:
-                Livro livro = new Livro();
-                livro.cadastraLivro();
-                break;
-            case 6:
-                Usuario usuario = new Usuario();
-                usuario.cadastraUsuario();
-                break;
-            case 7:
-                cadastraAdmin(nome, senha);
-                break;
-            case 8:
-                System.out.println("bye bye");
-                System.exit(0);
-                break;
-        }
+            System.out.println("");
+            System.out.println("1-Emprestimo");
+            System.out.println("2-Devolução");
+            System.out.println("3-Reserva");
+            System.out.println("4-Cadastrar Exemplar");
+            System.out.println("5-Cadastrar Livro");
+            System.out.println("6-Cadastrar Usuário");
+            System.out.println("7-Cadastrar Admin");
+            System.out.println("8-Sair");
+            System.out.println("");
+            do {
+                System.out.println("Escolha a opção desejada");
+                resp = leia.nextInt();
+            } while (resp < 1 || resp > 8);
+            switch (resp) {
+                case 1:
+                    Emprestimo emprestimo = new Emprestimo();
+                    emprestimo.fazEmprestimo();
+                    break;
+                case 2:
+                    Devolucao devolucao = new Devolucao();
+                    devolucao.fazDevolucao();
+                    break;
+                case 3:
+                    Reserva reserva = new Reserva();
+                    reserva.addUsuarioLista();
+                    break;
+                case 4:
+                    Exemplar exemplar = new Exemplar();
+                    exemplar.cadastraExemplar();
+                    break;
+                case 5:
+                    Livro livro = new Livro();
+                    livro.cadastraLivro();
+                    break;
+                case 6:
+                    Usuario usuario = new Usuario();
+                    usuario.cadastraUsuario();
+
+                    break;
+                case 7:
+                    cadastraAdmin(nome, senha);
+                    break;
+                case 8:
+                    System.out.println("bye bye");
+                    System.exit(0);
+                    break;
+            }
+        } while (resp != 8);
     }
     static String nome = "";
     static String senha = "";
 
-    public static void principal() {
+    public static void principal(boolean cadastro) {
         listaAdmin.add("juca");
         senhaAdmin.add("1234");
         Scanner leia = new Scanner(System.in);
         System.out.println("Sistema de biblioteca");
         int aux = 0;
-        do {
-            System.out.println("Se deseja fazer login digite 1, se deseja fazer o cadastro de um novo admin digite 2");
-            aux = leia.nextInt();
-        } while ((aux != 1) && (aux != 2));
-        String nome = "";
-        String senha = "";
+        if (cadastro) {
+            do {
+                System.out.println("Se deseja fazer login digite 1, se deseja fazer o cadastro de um novo admin digite 2");
+                aux = leia.nextInt();
+            } while ((aux != 1) && (aux != 2));
+            String nome = "";
+            String senha = "";
+        } else {
+            aux = 1;
+        }
         if (aux == 1) {
             leia = new Scanner(System.in);
 
             do {
+                System.out.println("FAÇA O LOGIN");
                 System.out.println("Informe o nome do admin");
                 nome = leia.nextLine();
                 System.out.println("Informe a senha");
@@ -137,6 +145,7 @@ public class Admin {
 
         } else {
             cadastraAdmin(nome, senha);
+            principal(false);
         }
 
         menu();
--- src/BibliotecaPrograma/Usuario.java
+++ src/BibliotecaPrograma/Usuario.java
@@ -12,7 +12,7 @@ public class Usuario {
     static int numCadastro;
     static String cpfUsuario;
     static String email;
-    static int phone;
+    static String phone;
 
     static ArrayList<String> listaUsuario = new ArrayList();
 
@@ -31,46 +31,84 @@ public class Usuario {
         System.out.println("Informe o nome da pessoa a ser cadastrada:");
         usuario.nomeUsuario = leia.nextLine();
         MaskFormatter formato = new MaskFormatter();
-        String aux;
+        String cpf;
         char aux1;
         char aux2;
         char traco1;
         boolean erro = false;
+        char chara;
+        boolean auxboo = false;
         do {
+            auxboo = false;
             do {
                 System.out.println("Informe o CPF da respectiva pessoa:");
-                aux = leia.nextLine();
-                aux.trim();
-            } while (aux.isEmpty() || (aux.length() != 14));
-            aux1 = aux.charAt(3);
-            aux2 = aux.charAt(7);
-            traco1 = aux.charAt(11);
-
+                cpf = leia.nextLine();
+                cpf.trim();
+            } while (cpf.isEmpty() || (cpf.length() != 14));
+            aux1 = cpf.charAt(3);
+            aux2 = cpf.charAt(7);
+            traco1 = cpf.charAt(11);
+            for (int i = 0; i < cpf.length() - 1; i++) {
+                chara = cpf.charAt(i);
+                if (i == 3 || i == 7 || i == 11) {
+                    continue;
+                }
+                if (chara < 48 || chara > 57) {
+                    auxboo = true;
+                    System.out.println("apenas numeros");
+                    break;
+                }
+            }
             if (aux1 == ('.') && (aux2 == ('.')) && (traco1 == ('-'))) {
                 erro = true;
             } else {
                 erro = false;
             }
 
-        } while (!erro);
+        } while ((!erro) || (auxboo));
         String phone;
 
         do {
-            System.out.println("Informe o seu numero de seu celular(ex:(DDD)91234-5678)");
-            phone = leia.nextLine();
-            if(phone.length()== 15){
-             aux1 = phone.charAt(0);
-            aux2 = phone.charAt(4);
-            traco1 = phone.charAt(10);   
-            erro = false;
-            }else{erro = true;}
-            
-        } while (!((aux1 == '(') && (aux2 == (')')) && (traco1 == ('-')))|| erro);
+            do {
+                auxboo = false;
+                System.out.println("Informe o seu numero de seu celular(ex:(DDD)91234-5678)");
+                phone = leia.nextLine();
+                if (phone.length() == 15) {
+                    aux1 = phone.charAt(0);
+                    aux2 = phone.charAt(4);
+                    traco1 = phone.charAt(10);
+                    erro = false;
+                } else {
+                    erro = true;
+                }
 
-        System.out.println(aux);
+            } while (!((aux1 == '(') && (aux2 == (')')) && (traco1 == ('-'))) || erro);
 
+            for (int i = 0; i < phone.length() - 1; i++) {
+                chara = phone.charAt(i);
+                if (i == 0 || i == 4 || i == 10) {
+                    continue;
+                }
+                if (chara < 48 || chara > 57) {
+                    auxboo = true;
+                    System.out.println("apenas numeros");
+                    break;
+                }
+            }
+        } while (auxboo);
+
+        do {
+            System.out.println("digite o email");
+            usuario.email = leia.nextLine();
+        } while (usuario.email.equals("") || !usuario.email.contains("@"));
+
+        usuario.cpfUsuario = cpf;
+        usuario.phone = phone;
         usuario.numCadastro = randomiza();
+
+        System.out.print("seu número de cadastro é: ");
         System.out.println(numCadastro);
+        System.out.println("USUÁRIO CADASTRADO COM SUCESSO");
     }
 
 }
